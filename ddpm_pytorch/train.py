614f84ade7c24a76431db1d3ba0d25879cbc1b24
mikedev <mik3dev@gmail.com>
2022-08-15 21:08:32 +0200

    add accelerator and devices, fix save dir format

--- ddpm_pytorch/train.py
+++ ddpm_pytorch/train.py
@@ -1,6 +1,7 @@
 import hydra
 import pkg_resources
 from omegaconf import DictConfig
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 
@@ -11,9 +12,10 @@ def train(config: DictConfig):
     model: pl.LightningModule = hydra.utils.instantiate(config.model, variance_scheduler=scheduler)
     train_dataset: Dataset = hydra.utils.instantiate(config.dataset.train)
     val_dataset: Dataset = hydra.utils.instantiate(config.dataset.val)
+
     pin_memory = 'cuda' in config.device
     train_dl = DataLoader(train_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
     val_dl = DataLoader(val_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
-    ckpt_callback = pl.callbacks.ModelCheckpoint('./', every_n_train_steps=config.checkpoint_steps)
-    trainer = pl.Trainer(checkpoint_callback=ckpt_callback)
+    ckpt_callback = ModelCheckpoint('./',  monitor='loss/val_loss', every_n_train_steps=config.checkpoint_steps)
+    trainer = pl.Trainer(checkpoint_callback=ckpt_callback, accelerator=config.accelerator, devices=config.devices)
     trainer.fit(model, train_dl, val_dl)
