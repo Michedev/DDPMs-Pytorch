83af83b7742287f0b342a2c45721986c4c033a83
mikedev <mik3dev@gmail.com>
2022-08-22 23:17:19 +0200

    add ema

---
+++ ddpm_pytorch/ema.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+import pytorch_lightning as pl
+import torch
+
+
+class EMA(pl.Callback):
+
+    def __init__(self, decay_factor: float):
+        assert 0.0 <= decay_factor <= 1.0
+        self.decay_factor = decay_factor
+        self.dict_params = dict()
+
+    @torch.no_grad()
+    def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        for n, p in pl_module.named_parameters():
+            self.dict_params[n] = p
+
+    @torch.no_grad()
+    def on_train_batch_start(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        batch: Any,
+        batch_idx: int,
+        unused: int = 0,
+    ) -> None:
+        for n, p in pl_module.named_parameters():
+            self.dict_params[n] = self.dict_params[n] * self.decay_factor + p * (1.0 - self.decay_factor)
+            p[:] = self.dict_params[n]
--- ddpm_pytorch/train.py
+++ ddpm_pytorch/train.py
@@ -5,6 +5,8 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 
+from ema import EMA
+
 
 @hydra.main(pkg_resources.resource_filename("ddpm_pytorch", 'config'), 'train.yaml')
 def train(config: DictConfig):
@@ -17,7 +19,10 @@ def train(config: DictConfig):
     train_dl = DataLoader(train_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
     val_dl = DataLoader(val_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
     ckpt_callback = ModelCheckpoint('./', monitor='loss/val_loss')
-    trainer = pl.Trainer(callbacks=[ckpt_callback], accelerator=config.accelerator, devices=config.devices,
+    callbacks = [ckpt_callback]
+    if config.ema:
+        callbacks.append(EMA(config.ema_decay))
+    trainer = pl.Trainer(callbacks=callbacks, accelerator=config.accelerator, devices=config.devices,
                          gradient_clip_val=config.gradient_clip_val,
                          gradient_clip_algorithm=config.gradient_clip_algorithm)
     trainer.fit(model, train_dl, val_dl)
