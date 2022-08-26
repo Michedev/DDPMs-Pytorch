7f9cb8efcdf51ef39fdce28b192bfec1bfc3b676
Mikedev <mdv1994@gmail.com>
2022-08-26 23:54:44 +0200

    fixes generation, add missing v hyper parameter

--- ddpm_pytorch/model/classifier_free_ddpm.py
+++ ddpm_pytorch/model/classifier_free_ddpm.py
@@ -12,7 +12,8 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
 
     def __init__(self, denoiser_module: nn.Module, T: int,
                  w: float, p_uncond: float, width: int,
-                 height: int, input_channels: int, num_classes: int, logging_freq: int):
+                 height: int, input_channels: int, num_classes: int,
+                 logging_freq: int, v: float):
         """
         :param denoiser_module: The nn which computes the denoise step i.e. q(x_{t-1} | x_t, c)
         :param T: the amount of noising steps
@@ -24,7 +25,9 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
         :param input_channels: image input channels
         :param num_classes: number of classes
         :param logging_freq: frequency of logging loss function during training
+        :param v:
         """
+        assert 0.0 <= v <= 1.0, f'0.0 <= {v} <= 1.0'
         assert 0.0 <= w <= 1.0, f'0.0 <= {w} <= 1.0'
         assert 0.0 <= p_uncond <= 1.0, f'0.0 <= {p_uncond} <= 1.0'
         super().__init__()
@@ -32,6 +35,7 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
         self.denoiser_module = denoiser_module
         self.T = T
         self.w = w
+        self.v = v
 
         self.p_uncond = p_uncond
         self.mse = nn.MSELoss()
@@ -88,7 +92,7 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
         if get_intermediate_steps:
             steps = []
         z_t = torch.randn(batch_size, self.input_channels,  # start with random noise sampled from N(0, 1)
-                              self.width, self.height, device=self.device)
+                          self.width, self.height, device=self.device)
         for t in range(T - 1, -1, -1):
             if get_intermediate_steps:
                 steps.append(z_t)
@@ -98,6 +102,15 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
             else:
                 eps = (1 + self.w) * self(z_t, t ,c) - self.w * self(z_t, t, c)
             x_t = (z_t - self._sigma_t(t) * eps) / self._alpha_t(t)
+            if t > 0:
+                z_t = self._mu_t1_t_z_x(t, t-1, z_t, x_t) + \
+                      self._sigma_t1_t_z_x(t, t-1) ** (1 - self.v) * \
+                      self._sigma_t1_t_z_x(t, t-1) ** self.v * torch.randn_like(x_t)
+            else:
+                z_t = x_t
+        if get_intermediate_steps:
+            steps.append(z_t)
+        return z_t if not get_intermediate_steps else steps
 
     def _alpha_t(self, t: torch.Tensor) -> torch.Tensor:
         return self.betas[t].sigmoid()
@@ -111,4 +124,4 @@ class GaussianDDPMClassifierFreeGuidance(pl.LightningModule):
         return e_t_t1 * self._alpha_t(t1) / alpha_t * z + (1 - e_t_t1) * alpha_t * x
 
     def _sigma_t1_t_z_x(self, t1, t):
-        return (1 - (t - t1).exp()) * self._sigma_t(t1)
+        return (1 - (self.betas[t] - self.betas[t1]).exp()) * self._sigma_t(t1)
