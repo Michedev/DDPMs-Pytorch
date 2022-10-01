c30e2f36c5313001940c3a772004110677a9b9c5
Mikedev <mdv1994@gmail.com>
2022-10-01 20:32:22 +0200

    add tests and fixes for issue #1

---
+++ tests/test_unet.py
@@ -0,0 +1,14 @@
+import torch
+from ddpm_pytorch.model.unet import UNetTimeStep
+
+def test_unet_same_width_height():
+    x = torch.rand(2, 1, 28, 28)
+    unet = UNetTimeStep([1, 32, 32, 64, 128], [3, 3, 3, 3], [1, 1, 1, 1], [1, 1, 1, 1], True, [0.0, 0.0, 0.0, 0.0], 100)
+    x_recon, v = unet(x, torch.tensor([0.25]*2).view(-1))
+    assert x.shape == x_recon.shape
+
+def test_unet_different_width_height():
+    x = torch.rand(2, 3, 32, 35)
+    unet = UNetTimeStep([3, 32, 64, 128], [3, 3, 3], [1, 1, 1], [1, 1, 1], True, [0.0, 0.0, 0.0], 10)
+    x_recon, v = unet(x, torch.tensor([0.25, 0.5]).view(-1))
+    assert x.shape == x_recon.shape
