97f9789a1e942295b8aa1bddd0167d3e32148b05
Mikedev <mdv1994@gmail.com>
2023-03-16 12:34:52 +0100

    add comments

--- model/ddpm.py
+++ model/ddpm.py
@@ -49,88 +49,158 @@ class GaussianDDPM(pl.LightningModule):
         self.iteration = 0
 
     def forward(self, x: torch.FloatTensor, t: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Forward pass of the DDPM model.
+
+        Args:
+            x: Input image tensor.
+            t: Time step tensor.
+
+        Returns:
+            Tuple of predicted noise tensor and predicted variance tensor.
+        """
         return self.denoiser_module(x, t)
 
+
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        """
+        Training step of the DDPM model.
+
+        Args:
+            batch: Tuple of input image tensor and target tensor.
+            batch_idx: Batch index.
+
+        Returns:
+            Dictionary containing the loss.
+        """
         X, y = batch
         with torch.no_grad():
-            X = X * 2 - 1  # map image values from [0, 1] to [-1, 1]
-        t: torch.Tensor = torch.randint(0, self.T - 1, (X.shape[0],),
-                                        device=X.device)  # todo add importance sampling
+            # Map image values from [0, 1] to [-1, 1]
+            X = X * 2 - 1
+        # Sample a random time step t from 0 to T-1 for each image in the batch
+        t: torch.Tensor = torch.randint(0, self.T - 1, (X.shape[0],), device=X.device)  # todo add importance sampling
+        # Compute alpha_hat for the selected time steps
         alpha_hat = self.alphas_hat[t].reshape(-1, 1, 1, 1)
+        # Sample noise eps from a normal distribution with the same shape as X
         eps = torch.randn_like(X)
+        # Compute the intermediate image x_t from the original image X, alpha_hat, and eps
         x_t = x0_to_xt(X, alpha_hat, eps)  # go from x_0 to x_t with the formula
+        # Run the intermediate image x_t through the model to obtain predicted noise and scale vectors (pred_eps, v)
         pred_eps, v = self(x_t, t)
+        # Compute the loss for the predicted noise
         loss = 0.0
         noise_loss = self.mse(eps, pred_eps)
         loss = loss + noise_loss
+        # If using the VLB loss, compute the VLB loss and add it to the total loss
         if self.vlb:
             loss_vlb = self.lambda_variational * self.variational_loss(x_t, X, pred_eps, v, t).mean(dim=0).sum()
             loss = loss + loss_vlb
+        # If it's time to log the loss, log the total loss and optionally the noise and VLB losses
         if (self.iteration % self.logging_freq) == 0:
             self.log('loss/train_loss', loss, on_step=True)
             if self.vlb:
                 self.log('loss/train_loss_noise', noise_loss, on_step=True)
                 self.log('loss/train_loss_vlb', loss_vlb, on_step=True)
-            norm_params = sum(
-                [torch.norm(p.grad) for p in self.parameters() if
-                 hasattr(p, 'grad') and p.grad is not None])
+            # Log the norm of the gradients of the parameters
+            norm_params = sum([torch.norm(p.grad) for p in self.parameters() if hasattr(p, 'grad') and p.grad is not None])
             self.log('grad_norm', norm_params)
+        # Increment the iteration count
         self.iteration += 1
+        # Return the loss as a dictionary
         return dict(loss=loss)
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        # Generate images and log them for visualization
         if batch_idx == 0:
-            gen_images = self.generate(batch_size=16)
-            gen_images = torchvision.utils.make_grid(gen_images)
-            self.logger.experiment.add_image('gen_val_images', gen_images, self.current_epoch)
+            gen_images = self.generate(batch_size=16)  # Generate 16 images
+            gen_images = torchvision.utils.make_grid(gen_images)  # Convert to grid
+            self.logger.experiment.add_image('gen_val_images', gen_images, self.current_epoch)  # Log the images
+
+        # Unpack the batch into inputs X and ground truth y
         X, y = batch
+
+        # Normalize inputs to [-1, 1] range
         with torch.no_grad():
             X = X * 2 - 1
-        t: torch.Tensor = torch.randint(0, self.T - 1, (X.shape[0],),
-                                        device=X.device)  # todo replace this with importance sampling
+
+        # Sample a time step t uniformly from [0, T-1] for each sample in the batch
+        # TODO: Replace uniform sampling with importance sampling
+        t: torch.Tensor = torch.randint(0, self.T - 1, (X.shape[0],), device=X.device)
+
+        # Compute alpha_hat from the precomputed alphas_hat for the sampled t
         alpha_hat = self.alphas_hat[t].reshape(-1, 1, 1, 1)
+
+        # Sample a noise vector eps from the standard normal distribution with the same shape as X
         eps = torch.randn_like(X)
+
+        # Compute x_t, the input to the model at time step t
         x_t = x0_to_xt(X, alpha_hat, eps)
+
+        # Forward pass through the model to get predicted noise and v
         pred_eps, v = self(x_t, t)
+
+        # Compute the reconstruction loss between the input and the predicted noise
         loss = self.mse(eps, pred_eps)
+
+        # If using the variational lower bound (VLB), compute the VLB loss and add it to the reconstruction loss
         if self.vlb:
             eps_loss = loss
             self.log('loss/val_eps_loss', eps_loss, on_step=True)
-            loss_vlb = self.lambda_variational * self.variational_loss(x_t, X, pred_eps, v, t) \
-                .mean(dim=0).sum()
+            loss_vlb = self.lambda_variational * self.variational_loss(x_t, X, pred_eps, v, t).mean(dim=0).sum()
             self.log('loss/val_vlb_loss', loss_vlb, on_step=True)
             loss = loss + loss_vlb
+
+        # Log the total validation loss
         self.log('loss/val_loss', loss, on_step=True)
+
+        # Return the loss as a dictionary
         return dict(loss=loss)
 
     def variational_loss(self, x_t: torch.Tensor, x_0: torch.Tensor,
-                         model_noise: torch.Tensor, v: torch.Tensor, t: torch.Tensor):
+                        model_noise: torch.Tensor, v: torch.Tensor, t: torch.Tensor):
         """
         Compute variational loss for time step t
-        :param x_t: the image at step t obtained with closed form formula from x_0
-        :param x_0: the input image
-        :param model_noise: the unet predicted noise
-        :param v: the unet predicted coefficients for the variance
-        :param t: the time step
-        :return: the pixel-wise variational loss, with shape [batch_size, channels, width, height]
+        
+        Parameters:
+            - x_t (torch.Tensor): the image at step t obtained with closed form formula from x_0
+            - x_0 (torch.Tensor): the input image
+            - model_noise (torch.Tensor): the unet predicted noise
+            - v (torch.Tensor): the unet predicted coefficients for the variance
+            - t (torch.Tensor): the time step
+        
+        Returns:
+            - vlb (torch.Tensor): the pixel-wise variational loss, with shape [batch_size, channels, width, height]
         """
         vlb = 0.0
         t_eq_0 = (t == 0).reshape(-1, 1, 1, 1)
+        
+        # Compute variational loss for t=0 (i.e., first time step)
         if torch.any(t_eq_0):
             p = torch.distributions.Normal(mu_x_t(x_t, t, model_noise, self.alphas_hat, self.betas, self.alphas),
-                                           sigma_x_t(v, t, self.betas_hat, self.betas))
+                                        sigma_x_t(v, t, self.betas_hat, self.betas))
+            # Compute log probability of x_0 under the distribution p
+            # and add it to the variational lower bound
             vlb += - p.log_prob(x_0) * t_eq_0.float()
+            
         t_eq_last = (t == (self.T - 1)).reshape(-1, 1, 1, 1)
+        
+        # Compute variational loss for t=T-1 (i.e., last time step)
         if torch.any(t_eq_last):
             p = torch.distributions.Normal(0, 1)
             q = torch.distributions.Normal(sqrt(self.alphas_hat[t]) * x_0, (1 - self.alphas_hat[t]))
+            # Compute KL divergence between distributions p and q
+            # and add it to the variational lower bound
             vlb += torch.distributions.kl_divergence(q, p) * t_eq_last
+            
+        # Compute variational loss for all other time steps
         q = torch.distributions.Normal(mu_hat_xt_x0(x_t, x_0, t, self.alphas_hat, self.alphas, self.betas),
-                                       sigma_hat_xt_x0(t, self.betas_hat))  # q(x_{t-1} | x_t, x_0)
+                                    sigma_hat_xt_x0(t, self.betas_hat))  # q(x_{t-1} | x_t, x_0)
         p = torch.distributions.Normal(mu_x_t(x_t, t, model_noise, self.alphas_hat, self.betas, self.alphas).detach(),
-                                       sigma_x_t(v, t, self.betas_hat, self.betas))  # p(x_t | x_{t-1})
+                                    sigma_x_t(v, t, self.betas_hat, self.betas))  # p(x_t | x_{t-1})
+        # Compute KL divergence between distributions p and q
+        # and add it to the variational lower bound
         vlb += torch.distributions.kl_divergence(q, p) * (~t_eq_last).float() * (~t_eq_0).float()
+        
         return vlb
 
     def configure_optimizers(self):
--- train.py
+++ train.py
@@ -12,44 +12,69 @@ import os
 from callbacks.ema import EMA
 
 
+# This function is the entry point for the training script. It takes a DictConfig object as an argument, which contains
+# the configuration for the training run. The configuration is loaded from a YAML file using Hydra.
 @hydra.main(pkg_resources.resource_filename("ddpm_pytorch", 'config'), 'train.yaml')
 def train(config: DictConfig):
+    # Initialize checkpoint to None
     ckpt = None
+
+    # Set random seeds for reproducibility
     pl.seed_everything(config.seed)
+
+    # If a checkpoint is specified in the config, load it and update the config accordingly
     if config.ckpt is not None:
-        os.chdir(Path(__file__).parent.parent.abspath())
-        assert Path(config.ckpt).exists()
-        ckpt = Path(config.ckpt)
-        config = OmegaConf.load(ckpt.parent / 'config.yaml')
-        os.chdir(ckpt.parent.abspath())
+        # Change the current working directory to the parent directory of the checkpoint file
+        os.chdir(os.path.dirname(config.ckpt))
+
+        # Assert that the checkpoint file exists
+        assert os.path.exists(config.ckpt)
+
+        # Set ckpt to the path of the checkpoint file
+        ckpt = config.ckpt
+
+        # Load the configuration file associated with the checkpoint file
+        config = OmegaConf.load(os.path.join(os.path.dirname(ckpt), 'config.yaml'))
+
+    # Save the updated configuration to a file called 'config.yaml'
     with open('config.yaml', 'w') as f:
         omegaconf.OmegaConf.save(config, f)
+
+    # Create the variance scheduler and a deep generative model using Hydra
     scheduler = hydra.utils.instantiate(config.scheduler)
     model: pl.LightningModule = hydra.utils.instantiate(config.model, variance_scheduler=scheduler)
+
+    # Create training and validation datasets using Hydra
     train_dataset: Dataset = hydra.utils.instantiate(config.dataset.train)
     val_dataset: Dataset = hydra.utils.instantiate(config.dataset.val)
 
+    # If a checkpoint is specified, load the model weights from the checkpoint
     if ckpt is not None:
-        model.load_from_checkpoint(ckpt.basename(), variance_scheduler=scheduler)
+        model.load_from_checkpoint(ckpt, variance_scheduler=scheduler)
 
+    # Save the hyperparameters of the model to a file called 'hparams.yaml'
     model.save_hyperparameters(OmegaConf.to_object(config)['model'])
 
+    # Create PyTorch dataloaders for the training and validation datasets
     pin_memory = 'gpu' in config.accelerator
     train_dl = DataLoader(train_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
     val_dl = DataLoader(val_dataset, batch_size=config.batch_size, pin_memory=pin_memory)
-    ckpt_callback = ModelCheckpoint('./', 'epoch={epoch}-valid_loss={loss/valid_loss_epoch}', monitor='loss/valid_loss_epoch',
-                                    auto_insert_metric_name=False, save_last=True)
-    callbacks = [ckpt_callback]
-    if config.ema:
-        callbacks.append(EMA(config.ema_decay))
-    if config.early_stop:
-        callbacks.append(EarlyStopping('loss/valid_loss_epoch', min_delta=config.min_delta,
-                                       patience=config.patience))
-    trainer = pl.Trainer(callbacks=callbacks, accelerator=config.accelerator, devices=config.devices,
-                         gradient_clip_val=config.gradient_clip_val,
-                         gradient_clip_algorithm=config.gradient_clip_algorithm)
-    trainer.fit(model, train_dl, val_dl, )
 
+    # Create a ModelCheckpoint callback that saves the model weights to disk during training
+    ckpt_callback = ModelCheckpoint('./', 'epoch={epoch}-valid_loss={loss/valid_loss_epoch}', 
+                                     monitor='loss/valid_loss_epoch', auto_insert_metric_name=False, save_last=True)
+    callbacks = [ckpt_callback]
+
+    # Add additional callbacks if specified in the configuration file
+    if config.ema:
+        # Create an Expontential Moving Average callback
+        callbacks.append(EMA(config.ema_decay))  
+    if config.early_stop:
+        callbacks.append(EarlyStopping('loss/valid_loss_epoch', min_delta=config.min_delta, patience=config.patience))
+
+    trainer = pl.Trainer(callbacks=callbacks, accelerator=config.accelerator, devices=config.devices,
+                         gradient_clip_val=config.gradient_clip_val, gradient_clip_algorithm=config.gradient_clip_algorithm)
+    trainer.fit(model, train_dl, val_dl)
 
 if __name__ == '__main__':
     train()
