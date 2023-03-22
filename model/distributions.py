5be1a5018bbd539a81958b777a98502a5b78625a
mikedev <mik3dev@gmail.com>
2023-03-22 17:35:53 +0100

    add smoothing constant to avoid nan values

--- model/distributions.py
+++ model/distributions.py
@@ -3,8 +3,7 @@ from typing import Optional
 import torch
 
 
-def mu_x_t(x_t: torch.Tensor, t: torch.Tensor, model_noise: torch.Tensor, alphas_hat: torch.Tensor, betas: torch.Tensor,
-           alphas: torch.Tensor) -> torch.Tensor:
+def mu_x_t(x_t: torch.Tensor, t: torch.Tensor, model_noise: torch.Tensor, alphas_hat: torch.Tensor, betas: torch.Tensor, alphas: torch.Tensor, eps: float = 1e-5) -> torch.Tensor:
     """
 
     :param x_t: the noised image
@@ -15,8 +14,8 @@ def mu_x_t(x_t: torch.Tensor, t: torch.Tensor, model_noise: torch.Tensor, alphas
     :param alphas: sequence of $\alpha$ used for variance scheduling
     :return: the mean of $q(x_t | x_0)$
     """
-    x = 1 / torch.sqrt(alphas[t].reshape(-1, 1, 1, 1)) * (x_t - (
-                betas[t].reshape(-1, 1, 1, 1) / torch.sqrt(1 - alphas_hat[t].reshape(-1, 1, 1, 1)) * model_noise))
+    x = 1 / (torch.sqrt(alphas[t].reshape(-1, 1, 1, 1) + eps) + eps) * (x_t - (
+                betas[t].reshape(-1, 1, 1, 1) / (torch.sqrt(1 - alphas_hat[t].reshape(-1, 1, 1, 1) + eps) + eps) * model_noise))
     # tg.guard(x, "B, C, W, H")
     return x
 
@@ -30,13 +29,12 @@ def sigma_x_t(v: torch.Tensor, t: torch.Tensor, betas_hat: torch.Tensor, betas: 
     :param betas: sequence of $\beta$ used for variance scheduling
     :return: the estimated variance at time step t
     """
-    x = torch.exp(v * torch.log(betas[t].reshape(-1, 1, 1, 1)) + (1 - v) * torch.log(betas_hat[t].reshape(-1, 1, 1, 1)))
+    x = torch.exp(v * torch.log(betas[t].reshape(-1, 1, 1, 1) + eps) + (1 - v) * torch.log(betas_hat[t].reshape(-1, 1, 1, 1) + eps))
     # tg.guard(x, "B, C, W, H")
     return x
 
 
-def mu_hat_xt_x0(x_t: torch.Tensor, x_0: torch.Tensor, t: torch.Tensor, alphas_hat: torch.Tensor, alphas: torch.Tensor,
-                 betas: torch.Tensor):
+def mu_hat_xt_x0(x_t: torch.Tensor, x_0: torch.Tensor, t: torch.Tensor, alphas_hat: torch.Tensor, alphas: torch.Tensor, betas: torch.Tensor, eps: float = 1e-5) -> torch.Tensor:
     """
     Compute $\hat{mu}(x_t, x_0)$ of $q(x_{t-1} | x_t, x_0)$ from "Improving Denoising Diffusion probabilistic Models", eqn 11 page 2
     :param x_t: The noised image at step t
@@ -47,22 +45,22 @@ def mu_hat_xt_x0(x_t: torch.Tensor, x_0: torch.Tensor, t: torch.Tensor, alphas_h
     :param betas: sequence of $\beta$ used for variance scheduling [T}
     :return: the mean of distribution $q(x_{t-1} | x_t, x_0)$
     """
-    one_min_alpha_hat = (1 - alphas_hat[t].reshape(-1, 1, 1, 1)) + 1e-5
-    x = torch.sqrt(alphas_hat[t - 1].reshape(-1, 1, 1, 1)) * betas[t].reshape(-1, 1, 1, 1) / one_min_alpha_hat * x_0 + \
-        torch.sqrt(alphas[t].reshape(-1, 1, 1, 1)) * (
+    one_min_alpha_hat = (1 - alphas_hat[t].reshape(-1, 1, 1, 1)) + eps
+    x = torch.sqrt(alphas_hat[t - 1].reshape(-1, 1, 1, 1) + eps) * betas[t].reshape(-1, 1, 1, 1) / one_min_alpha_hat * x_0 + \
+        torch.sqrt(alphas[t].reshape(-1, 1, 1, 1) + eps) * (
                     1 - alphas_hat[t - 1].reshape(-1, 1, 1, 1)) / one_min_alpha_hat * x_t
     # tg.guard(x, "B, C, W, H")
     return x
 
 
-def sigma_hat_xt_x0(t: torch.Tensor, betas_hat: torch.Tensor) -> torch.Tensor:
+def sigma_hat_xt_x0(t: torch.Tensor, betas_hat: torch.Tensor, eps: float = 1e-5) -> torch.Tensor:
     """
     Compute the variance of of $q(x_{t-1} | x_t, x_0)$ from "Improving Denoising Diffusion probabilistic Models", eqn 12 page 2
     :param t: the time step [batch_size]
     :param betas_hat: the array of beta hats [T]
     :return: the variance at time step t as scalar [batch_size, 1, 1, 1]
     """
-    return betas_hat[t].reshape(-1, 1, 1, 1)
+    return (betas_hat[t].reshape(-1, 1, 1, 1) + eps)
 
 
 def x0_to_xt(x_0: torch.Tensor, alpha_hat_t: torch.Tensor, eps: Optional[torch.Tensor] = None) -> torch.Tensor:
