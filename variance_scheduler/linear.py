7d6eacdfbf2870d74d0bbedb0daff0f2021d088c
Mikedev <mdv1994@gmail.com>
2023-05-27 15:41:51 +0200

    replace anaconda-project with conda-project

--- variance_scheduler/linear.py
+++ variance_scheduler/linear.py
@@ -3,8 +3,19 @@ import torch
 
 
 class LinearScheduler(Scheduler):
+    """
+    A scheduler that linearly interpolates between two values of beta over T steps.
+    """
 
     def __init__(self, T: int, beta_start: float, beta_end: float):
+        """
+        Initializes the LinearScheduler.
+
+        Args:
+            T (int): The number of steps to interpolate over.
+            beta_start (float): The starting value of beta.
+            beta_end (float): The ending value of beta.
+        """
         self.T = T
         self.beta_start = beta_start
         self.beta_end = beta_end
@@ -16,13 +27,25 @@ class LinearScheduler(Scheduler):
         self._beta_hat = (1 - self._alpha_hat_t_minus_1) / (1 - self._alpha_hat) * self._beta
 
     def get_alpha_hat(self):
+        """
+        Returns the cumulative product of (1 - beta) over time.
+        """
         return self._alpha_hat
 
     def get_alphas(self):
+        """
+        Returns the values of alpha over time.
+        """
         return self._alpha
 
     def get_betas(self):
+        """
+        Returns the values of beta over time.
+        """
         return self._beta
 
     def get_betas_hat(self):
+        """
+        Returns the values of beta_hat over time.
+        """
         return self._beta_hat
